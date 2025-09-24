3e433643bcfcce08ec90c4abedcae80a02534f36
alriffaud <alriffaud@gmail.com>
2025-09-24 09:32:28 -0300

    Creation of 6-plot_churn_distribution.py file.

---
+++ data_analysis/data_preparation_visualization/6-plot_churn_distribution.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Module for plotting churn distribution.
+"""
+import matplotlib.pyplot as plt
+
+
+def plot_churn_distribution(df):
+    """
+    Visualizes churn class distribution.
+    Args:
+        df (pd.DataFrame): DataFrame with a 'Churn' column.
+    Returns:
+        None
+    """
+    plt.figure(figsize=(12, 8))
+
+    churn_counts = df['Churn'].value_counts()
+
+    plt.bar(churn_counts.index, churn_counts.values,
+            color=['skyblue', 'coral'])
+
+    plt.title("Churn Distribution")
+    plt.xlabel("Churn")
+    plt.ylabel("Count")
+    plt.show()
