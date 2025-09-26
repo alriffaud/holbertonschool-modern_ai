63ba7a7cd2afe343305558a34e76bce8201cc10f
alriffaud <alriffaud@gmail.com>
2025-09-26 15:40:06 -0300

    Creation of 9-plot_correlation_heatmap.py file.

---
+++ data_analysis/data_preparation_visualization/9-plot_correlation_heatmap.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Plot Correlation Heatmap
+"""
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+def plot_correlation_heatmap(df):
+    """
+    Visualizes correlations between continuous numeric features using seaborn
+    heatmap.
+    Args:
+        df: pandas DataFrame
+    """
+    plt.figure(figsize=(6, 5))
+
+    # Compute the correlation matrix
+    numeric_cols = df.select_dtypes(include=['number']).columns.tolist()
+    corr = df[numeric_cols].corr()
+
+    # Generate a heatmap
+    sns.heatmap(corr, annot=True, fmt=".2f", cmap="coolwarm", square=True,
+                vmin=-1, vmax=1, cbar_kws={"shrink": .8})
+    plt.title("Correlation Matrix")
+    plt.show()
