5533b88010514b9128332d93308eddbf2152ccfc
alriffaud <alriffaud@gmail.com>
2025-11-08 09:15:25 -0300

    Creation of 3-Lasso_regression.py file.

---
+++ machine_learning/linear_models/3-Lasso_regression.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Creates and returns a Lasso Regression model using Scikit-learn."""
+from sklearn import linear_model
+
+
+def lasso_regression(random_state):
+    """
+    Creates and returns a Lasso Regression model using Scikit-learn.
+    Args:
+        random_state (int): Seed for reproducibility.
+    Returns:
+        model: An untrained Lasso regression model instance.
+    """
+    model = linear_model.Lasso(alpha=1.0,
+                               random_state=random_state,
+                               max_iter=10000)
+    return model
